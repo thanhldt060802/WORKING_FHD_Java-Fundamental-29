d04ed04c71efe3483db80a23a6daaf4e503ce9d2
Le Dao Tan Thanh <thanhldt060802@gmail.com>
2026-04-22 00:10:31 +0700

    Update code Day 45

--- buoi_45/src/main/java/com/thanhldt/buoi_45.sql
+++ buoi_45/src/main/java/com/thanhldt/buoi_45.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE school_management;
+
+DROP DATABASE IF EXISTS school_management;
+CREATE DATABASE school_management;
+USE school_management;
+
+CREATE TABLE students  -- Bảng sinh viên
+(
+	student_id VARCHAR(255),  -- Mã số sinh viên
+	student_name VARCHAR(255),  -- Tên sinh viên
+	age INT,  -- Tuổi
+	PRIMARY KEY (student_id)
+);
+
+INSERT INTO students (student_id, student_name, age) VALUES
+('S01', 'A', 29),
+('S02', 'B', 25),
+('S03', 'C', 21);
